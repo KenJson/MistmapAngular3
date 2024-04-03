40b5ee069df11e13b05c00a29343264473a8dba7
KenJson <chraitikojima.kenji@gmail.com>
2024-04-03 19:10:03 +0200

    rased lots of comments

--- src/app/components/mapbox/mapbox.component.ts
+++ src/app/components/mapbox/mapbox.component.ts
@@ -73,6 +73,142 @@ export class MapboxComponent implements OnInit {
 
   ) { }
 
+  ngOnInit() {
+    mapboxgl.accessToken = this.mapboxKeyService.getMapboxKey();
+
+    this.authState().subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
+
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat]
+    });
+
+    this.leylineService.setMap(this.map);
+    const app = initializeApp(environment.firebase);
+    const analytics = getAnalytics(app);
+    this.mapboxCtrlsService.addNavigationControl(this.map, 'top-right');
+    this.mapboxCtrlsService.addGeolocateControl(this.map, 'top-right');
+
+
+
+    this.map.on('load', () => {
+      if (this.map) {
+        this.map.addSource('line1', {
+          type: 'geojson',
+          data: './assets/leyline_menhir.geojson'
+        });
+
+        this.map.addLayer({
+          id: 'line-layer1',
+          type: 'line',
+          source: 'line1',
+          layout: {},
+          paint: {
+            'line-color': '#FF0000',
+            'line-width': 8
+          }
+        });
+      }
+
+
+      if (this.map) {
+        this.map.addSource('line2', {
+          type: 'geojson',
+          data: './assets/leyline_villages.geojson'
+        });
+      }
+
+      if (this.map) {
+        this.map.addLayer({
+          id: 'line-layer2',
+          type: 'line',
+          source: 'line2',
+          layout: {},
+          paint: {
+            'line-color': '#0000FF',
+            'line-width': 8
+          }
+        });
+      }
+
+
+
+
+      this.mapLayerService.getManifest().subscribe(manifest => {
+        manifest.forEach(entry => {
+          if (this.map) {
+
+            const id = entry.geojson.split('.')[0];
+            this.mapLayerService.addSource(this.map, id, 'geojson', `assets/Geojsons/${entry.geojson}`);
+
+
+            this.map.loadImage(`assets/icons/${entry.icon}`, (error, image) => {
+              if (error) throw error;
+
+              if (this.map && image) {
+                this.map.addImage(id, image);
+
+                this.map.addLayer({
+                  id: id,
+                  type: 'symbol',
+                  source: id,
+                  layout: {
+                    'icon-image': id,
+                    'icon-size': 0.2
+                  }
+                });
+
+
+
+
+
+
+                this.map.on('click', id, (e) => {
+                  console.log('Feature clicked:', e);
+                  if (e.features && e.features.length > 0) {
+
+                    const properties = e.features[0].properties as FeatureProperties;
+
+                    if (this.map) {
+                      new mapboxgl.Popup()
+                        .setLngLat(e.lngLat)
+                        .setHTML(`
+                          <h3>${properties.Name}</h3>
+                          <p>${properties.description}</p>
+                          
+                        `)
+                        .addTo(this.map);
+
+                    }
+                  }
+                });
+
+
+              }
+            });
+          }
+        }
+
+        );
+        this.leylineService.moveLayerToTop('leyline');
+
+
+      });
+
+
+
+
+
+
+
+
+    });
+  };
 
   goToProfile() {
     this.router.navigate(['/profile']);
@@ -123,161 +259,7 @@ export class MapboxComponent implements OnInit {
     return user(this._auth);
   }
 
-  ngOnInit() {
-    mapboxgl.accessToken = this.mapboxKeyService.getMapboxKey();
 
-    this.authState().subscribe((user) => {
-      this.isLoggedIn = !!user;
-    });
-
-
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: 13,
-      center: [this.lng, this.lat]
-    });
-
-    this.leylineService.setMap(this.map);
-    const app = initializeApp(environment.firebase);
-    const analytics = getAnalytics(app);
-    this.mapboxCtrlsService.addNavigationControl(this.map, 'top-right');
-    this.mapboxCtrlsService.addGeolocateControl(this.map, 'top-right');
-
-    /*
-    // Uncomment this if you have the MapboxDirections module
-    this.mapboxCtrlsService.addDirectionsControl(this.map, 'top-left');
-*/
-    //test zone for lines 
-
-    this.map.on('load', () => {
-      if (this.map) {
-        this.map.addSource('line1', {
-          type: 'geojson',
-          data: './assets/leyline_menhir.geojson'
-        });
-
-        this.map.addLayer({
-          id: 'line-layer1',
-          type: 'line',
-          source: 'line1',
-          layout: {},
-          paint: {
-            'line-color': '#FF0000',
-            'line-width': 8
-          }
-        });
-      }
-
-
-      if (this.map) {
-        this.map.addSource('line2', {
-          type: 'geojson',
-          data: './assets/leyline_villages.geojson'
-        });
-      }
-
-      if (this.map) {
-        this.map.addLayer({
-          id: 'line-layer2',
-          type: 'line',
-          source: 'line2',
-          layout: {},
-          paint: {
-            'line-color': '#0000FF',
-            'line-width': 8
-          }
-        });
-      }
-
-
-
-      //test zone for lines
-
-      this.mapLayerService.getManifest().subscribe(manifest => {
-        manifest.forEach(entry => {
-          if (this.map) {
-            //    this.mapboxDirectionsService.initialize(this.map, mapboxgl.accessToken);
-            const id = entry.geojson.split('.')[0]; // Use the filename without the extension as the ID
-            this.mapLayerService.addSource(this.map, id, 'geojson', `assets/Geojsons/${entry.geojson}`);
-
-            // Use the icon property of the entry to form the path to the icon image
-            this.map.loadImage(`assets/icons/${entry.icon}`, (error, image) => {
-              if (error) throw error;
-
-              // Add the image to the map
-              if (this.map && image) {
-                this.map.addImage(id, image);
-
-                // Add the layer using the image as the icon
-                this.map.addLayer({
-                  id: id,
-                  type: 'symbol',
-                  source: id,
-                  layout: {
-                    'icon-image': id,
-                    'icon-size': 0.2
-                  }
-                });
-
-                //   const uniqueId = `${id}-${Date.now()}`;
-
-
-
-
-                this.map.on('click', id, (e) => {
-                  console.log('Feature clicked:', e);
-                  if (e.features && e.features.length > 0) {
-
-                    const properties = e.features[0].properties as FeatureProperties;
-
-                    if (this.map) {
-                      new mapboxgl.Popup()
-                        .setLngLat(e.lngLat)
-                        .setHTML(`
-                          <h3>${properties.Name}</h3>
-                          <p>${properties.description}</p>
-                          
-                        `)
-                        .addTo(this.map);
-
-                    }
-                  }
-                });
-
-
-              }
-            });
-          }
-        }
-
-        );
-        this.leylineService.moveLayerToTop('leyline');
-
-
-      });
-
-
-
-
-
-      /*
-            // Add directions control to the map
-           const directions = new MapboxDirections({
-               accessToken: mapboxgl.accessToken,
-               unit: 'metric',
-         
-             });
-             if (this.map) {
-               this.map.addControl(directions, 'top-left');
-             }
-          
-      */
-
-
-
-    });
-  };
 
   loadLeyline(filename: string) {
     this.leylineService.loadGeojson(filename).then(() => {
